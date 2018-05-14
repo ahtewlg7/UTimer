f3b1eb91067e245148d6be7c587c030a500dd0a5
lw <184333220@qq.com>
2018-05-14 23:40:27 +0800

    补提代码

--- LibAhtewlg7/src/main/java/ahtewlg7/utimer/mvp/IRecyclerViewMvpV.java
+++ LibAhtewlg7/src/main/java/ahtewlg7/utimer/mvp/IRecyclerViewMvpV.java
@@ -1,23 +1,16 @@
 package ahtewlg7.utimer.mvp;
 
-import android.support.annotation.NonNull;
-
 import java.util.List;
-
-import ahtewlg7.utimer.entity.view.BaseSectionEntity;
-import io.reactivex.Flowable;
 
 /**
  * Created by lw on 2018/3/8.
  */
 
 public interface IRecyclerViewMvpV<T> {
-    public void initRecyclerView(List<BaseSectionEntity> dataList);
-    public void resetRecyclerView(List<BaseSectionEntity> dataList);
+    public void initRecyclerView(List<T> dataList);
+    public void resetRecyclerView(List<T> dataList);
 
     public void onRecyclerViewInitStart();
     public void onRecyclerViewInitErr();
     public void onRecyclerViewInitEnd();
-
-    public Flowable<BaseSectionEntity> mapBean(@NonNull Flowable<T> sourceBeanFlowable);
 }
