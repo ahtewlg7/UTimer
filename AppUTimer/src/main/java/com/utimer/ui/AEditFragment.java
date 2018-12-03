b28fb068ab58fedec875fc4b4ab69e706d423cee
lw <184333220@qq.com>
2018-12-03 22:35:06 +0800

    添加shorthand 编辑界面逻辑

---
+++ AppUTimer/src/main/java/com/utimer/ui/AEditFragment.java
@@ -0,0 +1,121 @@
+package com.utimer.ui;
+
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.common.base.Optional;
+import com.google.common.collect.Maps;
+import com.jakewharton.rxbinding2.widget.RxTextView;
+
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
+
+import ahtewlg7.utimer.util.Logcat;
+import io.reactivex.Observable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
+import io.reactivex.functions.Predicate;
+import io.reactivex.subjects.PublishSubject;
+
+/**
+ * Created by lw on 2018/11/8.
+ */
+public abstract class AEditFragment extends AToolbarBkFragment{
+    public static final String TAG = AEditFragment.class.getSimpleName();
+
+    protected abstract void onEditMode(int index, Observable<String> rawTxtRx);
+
+    protected boolean isParsing;
+    protected int preEditPosition = -1;
+    protected Disposable editViewDisposable;
+    protected HashMap<Integer, EditViewBean> editViewMap;
+    protected PublishSubject<EditViewBean> editViewPublishSubject;
+
+    @Override
+    public void onViewCreated(View inflateView) {
+        super.onViewCreated(inflateView);
+        editViewPublishSubject = PublishSubject.create();
+        editViewMap            = Maps.newHashMap();
+        createEditViewListen();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        toStopEditWatch();
+    }
+
+    protected void createEditViewListen(){
+        editViewDisposable = editViewPublishSubject.subscribe(new Consumer<EditViewBean>() {
+            @Override
+            public void accept(EditViewBean editViewBean) throws Exception {
+                editViewMap.put(editViewBean.getIndex(), editViewBean);
+                onEditMode(editViewBean.getIndex(), getTextWatcher(editViewBean.getEditTextView()));
+            }
+        });
+    }
+
+    protected Observable<String> getTextWatcher(EditText editText){
+        return RxTextView.textChanges(editText).throttleLast(10, TimeUnit.SECONDS)
+                .filter(new Predicate<CharSequence>() {
+                    @Override
+                    public boolean test(CharSequence charSequence) throws Exception {
+                        return !isParsing;
+                    }
+                })
+                .map(new Function<CharSequence, String>() {
+                    @Override
+                    public String apply(CharSequence charSequence) throws Exception {
+                        return charSequence.toString();
+                    }
+                });
+    }
+
+    protected void toStopEditWatch(){
+        if(editViewDisposable != null && !editViewDisposable.isDisposed())
+            editViewDisposable.dispose();
+        editViewMap.clear();
+        editViewDisposable = null;
+    }
+    protected void onEditViewLock(EditViewBean editViewBean){
+        boolean isEditing = editViewBean.isEditing();
+        Logcat.i(TAG,"onEditViewLock isEditing = " + isEditing);
+        editViewBean.getEditTextView().setFocusable(isEditing);
+        editViewBean.getEditTextView().setFocusableInTouchMode(isEditing);
+    }
+    protected Optional<EditViewBean> onEditViewLock(int position){
+        if(position < 0 || position >= editViewMap.size()) {
+            Logcat.i(TAG,"onEditViewLock : position invalid");
+            return Optional.absent();
+        }
+        return Optional.fromNullable(editViewMap.get(position));
+    }
+
+    class EditViewBean{
+        private int index;
+        private boolean isEditing;
+        private EditText editTextView;
+
+        public EditViewBean(int index, EditText editTextView) {
+            this.index = index;
+            this.editTextView = editTextView;
+        }
+
+        public boolean isEditing() {
+            return isEditing;
+        }
+
+        public void setEditing(boolean editing) {
+            isEditing = editing;
+        }
+
+        public int getIndex() {
+            return index;
+        }
+
+        public EditText getEditTextView() {
+            return editTextView;
+        }
+    }
+}
---
+++ AppUTimer/src/main/java/com/utimer/ui/ShortHandEditFragment.java
@@ -0,0 +1,223 @@
+package com.utimer.ui;
+
+import android.os.Bundle;
+import android.support.annotation.MenuRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.blankj.utilcode.util.ToastUtils;
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.google.common.base.Optional;
+import com.trello.rxlifecycle2.LifecycleProvider;
+import com.utimer.R;
+
+import java.util.List;
+
+import ahtewlg7.utimer.entity.gtd.ShortHandEntity;
+import ahtewlg7.utimer.entity.md.EditElement;
+import ahtewlg7.utimer.entity.md.EditMementoBean;
+import ahtewlg7.utimer.factory.ShortHandFactory;
+import ahtewlg7.utimer.mvp.ShorthandEditMvpP;
+import ahtewlg7.utimer.util.Logcat;
+import ahtewlg7.utimer.util.MyRInfo;
+import ahtewlg7.utimer.view.EditLinerRecyclerView;
+import ahtewlg7.utimer.view.md.MdEditText;
+import butterknife.BindView;
+import io.reactivex.Observable;
+
+public class ShortHandEditFragment extends AEditFragment implements ShorthandEditMvpP.IShorthandEditMvpV,
+        BaseQuickAdapter.OnItemChildClickListener {
+    public static final String TAG = ShortHandEditFragment.class.getSimpleName();
+
+    private static final String KEY_SHORTHAND = "shorthand";
+
+    @BindView(R.id.fragment_shorthand_toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.fragment_shorthand_edit_rv)
+    EditLinerRecyclerView editReCyclerView;
+
+    private ShortHandEntity shorthandEntity;
+    private ShorthandEditMvpP editMvpP;
+
+    public static ShortHandEditFragment newInstance(ShortHandEntity entity) {
+        Bundle args = new Bundle();
+        if(entity != null)
+            args.putSerializable(KEY_SHORTHAND, entity);
+        else
+            args.putSerializable(KEY_SHORTHAND, ShortHandFactory.getInstance().newValue());
+        ShortHandEditFragment fragment = new ShortHandEditFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(false);
+    }
+
+    @Override
+    public void onViewCreated(View inflateView) {
+        super.onViewCreated(inflateView);
+
+        shorthandEntity = getShorthandEntity();
+
+        if(shorthandEntity == null || !shorthandEntity.ifValid()){
+            Logcat.i(TAG,"shorthandEntity no valid");
+            ToastUtils.showShort(R.string.entity_invalid);
+            pop();
+            return ;
+        }
+        initByArgument();
+    }
+
+    @Override
+    public int getLayoutRid() {
+        return R.layout.fragment_shorthand;
+    }
+
+    @Override
+    protected String getTitle() {
+        return MyRInfo.getStringByID(R.string.title_shorthand);
+    }
+
+    @Override
+    @MenuRes
+    protected int getMenuRid() {
+        return R.menu.tool_menu;
+    }
+
+    @NonNull
+    @Override
+    protected Toolbar getToolbar() {
+        return toolbar;
+    }
+
+    @Override
+    protected void initToolbar() {
+        toolbar.setTitle(getTitle());
+    }
+
+
+    @Override
+    protected void onEditMode(int index, Observable<String> rawTxtRx) {
+        Logcat.i(TAG,"onEditMode " + index);
+        editMvpP.toModify(index, rawTxtRx);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Logcat.i(TAG,"onOptionsItemSelected " + item.getTitle());
+        boolean result = false;
+        switch (item.getItemId()) {
+            case R.id.tool_menu_add:
+                Logcat.i(TAG, "to create shorthand");
+                break;
+            default:
+                result = super.onOptionsItemSelected(item);
+                break;
+        }
+        return result;
+    }
+
+    /**********************************************BaseQuickAdapter**********************************************/
+    @Override
+    public void onItemChildClick(BaseQuickAdapter adapter, View view,final int position) {
+        Logcat.i(TAG,"onItemChildClick position = " + position + ",preEditPosition = " + preEditPosition);
+        if(preEditPosition != position){
+            Optional<EditViewBean> editViewBeanOptional = onEditViewLock(preEditPosition);
+            if(editViewBeanOptional.isPresent()){
+                editViewBeanOptional.get().setEditing(false);
+                onEditViewLock(editViewBeanOptional.get());
+            }
+
+            MdEditText currEditText   = (MdEditText) adapter.getViewByPosition(position, R.id.view_md_edit_tv);
+            EditViewBean editViewBean = new EditViewBean(position, currEditText);
+            editViewBean.setEditing(true);
+            onEditViewLock(editViewBean);
+            editViewPublishSubject.onNext(editViewBean);
+            preEditPosition = position;
+        }
+    }
+    /**********************************************IShorthandEditMvpV**********************************************/
+    @Override
+    public void onLoadStart() {
+        ToastUtils.showShort("onLoadStart");
+    }
+
+    @Override
+    public void onLoadErr(Throwable e) {
+        ToastUtils.showShort("onLoadErr : " + e.getMessage());
+    }
+
+    @Override
+    public void onLoadEnd(List<EditElement> mdElementList) {
+        ToastUtils.showShort("onLoadEnd size = " + mdElementList.size());
+        editReCyclerView.init(getContext(), mdElementList, null,
+                this ,null, null);
+    }
+
+    @Override
+    public void onParseStart() {
+        ToastUtils.showShort("to start parse");
+    }
+
+    @Override
+    public void onParseErr(Throwable e) {
+        ToastUtils.showShort("parse err: " + e.getMessage());
+    }
+
+    @Override
+    public void onParseSucc(EditMementoBean MdMementoBean) {
+        ToastUtils.showShort("parse end");
+        editReCyclerView.resetData(MdMementoBean.getIndex(), MdMementoBean.getElementList());
+    }
+
+    @Override
+    public void onParseEnd() {
+
+    }
+
+    @Override
+    public void onRestoreEnd(EditMementoBean MdMementoBean) {
+
+    }
+
+    @Override
+    public void onSaveStart() {
+
+    }
+
+    @Override
+    public void onSaveErr(Throwable e) {
+
+    }
+
+    @Override
+    public void onSaveEnd() {
+
+    }
+
+    @Override
+    public void onActionCancel() {
+
+    }
+
+
+
+    @Override
+    public LifecycleProvider getRxLifeCycleBindView() {
+        return this;
+    }
+
+    private void initByArgument() {
+        editMvpP   = new ShorthandEditMvpP(shorthandEntity, this);
+        editMvpP.toLoadTxt();
+    }
+    private ShortHandEntity getShorthandEntity(){
+        return (ShortHandEntity) getArguments().getSerializable(KEY_SHORTHAND);
+    }
+}
