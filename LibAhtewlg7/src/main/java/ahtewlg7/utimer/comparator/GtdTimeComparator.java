2be914d8153e26aa70fd585127e16d128db233b9
lw <184333220@qq.com>
2019-04-20 19:47:54 +0800

    时间越靠近者优先

--- LibAhtewlg7/src/main/java/ahtewlg7/utimer/comparator/GtdTimeComparator.java
+++ LibAhtewlg7/src/main/java/ahtewlg7/utimer/comparator/GtdTimeComparator.java
@@ -1,8 +1,6 @@
 package ahtewlg7.utimer.comparator;
 
 
-import org.joda.time.DateTime;
-
 import java.util.Comparator;
 
 import ahtewlg7.utimer.enumtype.ComparatorType;
@@ -20,20 +18,20 @@ public class GtdTimeComparator<T extends ITimeComparator> implements Comparator<
     public GtdTimeComparator() {
         dateTimeAction = new DateTimeAction();
     }
-
+    //who close who first
     @Override
     public int compare(T o1, T o2) {
         int result = ComparatorType.GREATER.value();
-        if(!o1.getComparatorTime().isPresent() && o2.getComparatorTime().isPresent())
+        if(!o2.getComparatorTime().isPresent() && o1.getComparatorTime().isPresent())
             result = ComparatorType.LESS.value();
-        else if(o1.getComparatorTime().isPresent() && !o2.getComparatorTime().isPresent())
+        else if(o2.getComparatorTime().isPresent() && !o1.getComparatorTime().isPresent())
             result = ComparatorType.GREATER.value();
-        else if(!o1.getComparatorTime().isPresent() && !o2.getComparatorTime().isPresent())
+        else if(!o2.getComparatorTime().isPresent() && !o1.getComparatorTime().isPresent())
             result = ComparatorType.EQUAL.value();
         else{
-            if(((DateTime)o1.getComparatorTime().get()).isBefore((DateTime)o2.getComparatorTime().get()))
+            if((o2.getComparatorTime().get()).isBefore(o1.getComparatorTime().get()))
                 result = ComparatorType.GREATER.value();
-            else if(((DateTime)o1.getComparatorTime().get()).isEqual((DateTime)o2.getComparatorTime().get()))
+            else if((o2.getComparatorTime().get()).isEqual(o1.getComparatorTime().get()))
                 result = ComparatorType.EQUAL.value();
             else
                 result = ComparatorType.LESS.value();
