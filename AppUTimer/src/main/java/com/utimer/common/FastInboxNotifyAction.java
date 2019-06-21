a67efe01db53a00ad365264f501fe6a5fdef4ca4
lw <184333220@qq.com>
2019-06-21 23:40:58 +0800

    添加状态栏快捷输入逻辑

---
+++ AppUTimer/src/main/java/com/utimer/common/FastInboxNotifyAction.java
@@ -0,0 +1,121 @@
+package com.utimer.common;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.RemoteInput;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
+
+import com.google.common.collect.Lists;
+import com.utimer.R;
+
+import java.util.List;
+
+import ahtewlg7.utimer.state.GtdMachine;
+import ahtewlg7.utimer.util.AndrManagerFactory;
+import ahtewlg7.utimer.util.BroadcastAction;
+import ahtewlg7.utimer.util.MyRInfo;
+import ahtewlg7.utimer.util.NotifyAction;
+
+/**
+ * Created by lw on 2019/6/20.
+ */
+public class FastInboxNotifyAction {
+    public static final int ID_NOTIFY_FAST_INBOX            = 1;
+    public static final int VALUE_REQUEST_CODE_FAST_INBOX   = 1;
+    public static final String NAME_NOTIFY_FAST_INBOX       = MyRInfo.getStringByID(R.string.notify_remote_input_inbox);
+
+    public static final String RESULT_KEY_REMOTE_INPUT_INBOX = MyRInfo.getStringByID(R.string.notify_remote_input_inbox);
+    public static final String LABEL_REMOTE_INPUT_INBOX     = MyRInfo.getStringByID(R.string.prompt_fast_inbox);
+    public static final String LABEL_ACTION_INBOX           = MyRInfo.getStringByID(R.string.title_notify_action_fast_inbox);
+    public static final String TITLE_NOTIFY_INBOX           = MyRInfo.getStringByID(R.string.title_notify_fast_inbox);
+    public static final String DETAIL_NOTIFY_INBOX          = MyRInfo.getStringByID(R.string.prompt_inbox_msg);
+
+    public static final String INTENT_ACTION_FAST_INBOX     =   MyRInfo.getStringByID(R.string.intent_action_fast_inbox);
+
+    private boolean ifListening;
+    private Service service;
+    private NotifyAction notifyAction;
+    private NotificationManager notificationManager;
+    private NotifyIntentReceiver notifyIntentReceiver;
+
+    public FastInboxNotifyAction(Service service) {
+        this.service     = service;
+        notifyAction            = new NotifyAction();
+        notifyIntentReceiver    = new NotifyIntentReceiver(service);
+        notificationManager     = new AndrManagerFactory().getNotificationManager();
+    }
+
+    public boolean isIfListening() {
+        return ifListening;
+    }
+
+    public void toEnableFastInboxNotify(){
+        Notification notification = getNotification();
+        notification.flags |= Notification.FLAG_ONGOING_EVENT;
+        notifyAction.toStartNotify(ID_NOTIFY_FAST_INBOX, NAME_NOTIFY_FAST_INBOX, notification);
+    }
+    public void toDisableFastInboxNotify(){
+        notifyAction.toStopNotify(ID_NOTIFY_FAST_INBOX);
+    }
+
+    public void toStartListen() {
+        ifListening = notifyIntentReceiver.toStartListen();
+    }
+
+    public void toStopListen() {
+        ifListening = !notifyIntentReceiver.toStopListen();
+    }
+    private Notification getNotification(){
+        /*Notification.MessagingStyle messagingStyle = new Notification.MessagingStyle("老王");
+        messagingStyle.setConversationTitle("来自老宋");
+        builder.setStyle(messagingStyle);*/
+        RemoteInput remoteInput     = notifyAction.getRemoteInput(RESULT_KEY_REMOTE_INPUT_INBOX, LABEL_REMOTE_INPUT_INBOX);
+        PendingIntent intent        = notifyAction.getServiceIntent(VALUE_REQUEST_CODE_FAST_INBOX, new Intent(INTENT_ACTION_FAST_INBOX));
+        Notification.Action action  = notifyAction.getNotifyAction(R.mipmap.ic_launcher_round, LABEL_ACTION_INBOX, intent, remoteInput);
+        return notifyAction.getNotification(R.mipmap.ic_launcher_round, TITLE_NOTIFY_INBOX, DETAIL_NOTIFY_INBOX, true, action);
+    }
+
+    class NotifyIntentReceiver {
+        private List<String> actionList;
+        private BroadcastAction broadcastAction;
+        private NotifyBroadcastReceiver broadcastReceiver;
+
+        NotifyIntentReceiver(@NonNull ContextWrapper contextWrapper) {
+            actionList          = Lists.newArrayList();
+            broadcastAction     = new BroadcastAction();
+            broadcastReceiver   = new NotifyBroadcastReceiver();
+
+            actionList.add(INTENT_ACTION_FAST_INBOX);
+        }
+
+        boolean toStartListen() {
+            return broadcastAction.registerReceiver(service, broadcastReceiver, actionList);
+        }
+
+        boolean toStopListen() {
+            return broadcastAction.unregisterReceiver(service, broadcastReceiver);
+        }
+    }
+
+    class NotifyBroadcastReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Bundle remoteInput  = RemoteInput.getResultsFromIntent(intent);
+            if(INTENT_ACTION_FAST_INBOX.equals(intent.getAction())) {
+                String inputStuff = remoteInput.getString(RESULT_KEY_REMOTE_INPUT_INBOX);
+                if(!TextUtils.isEmpty(inputStuff))
+                    GtdMachine.getInstance().getCurrState(null).toInbox(inputStuff, inputStuff);
+                toDisableFastInboxNotify();
+                toEnableFastInboxNotify();
+            }
+        }
+    }
+}
--- AppUTimer/src/main/java/com/utimer/ui/BaseBinderService.java
+++ AppUTimer/src/main/java/com/utimer/ui/BaseBinderService.java
@@ -1,16 +1,6 @@
 package com.utimer.ui;
 
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
-import android.os.Build;
-import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.RemoteInput;
-
-import com.utimer.R;
+import com.utimer.common.FastInboxNotifyAction;
 
 import ahtewlg7.utimer.ui.BinderService;
 
@@ -18,51 +8,21 @@ import ahtewlg7.utimer.ui.BinderService;
  * Created by lw on 2019/6/20.
  */
 public class BaseBinderService extends BinderService {
-
-    private NotificationManager notificationManager;
-    private String notificationId = "channelId";
-    private String notificationName = "channelName";
+    private FastInboxNotifyAction notifyAction;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-            NotificationChannel channel = new NotificationChannel(notificationId, notificationName, NotificationManager.IMPORTANCE_HIGH);
-            notificationManager.createNotificationChannel(channel);
-        }
-        this.startForeground(1,getNotification());
+
+        notifyAction = new FastInboxNotifyAction(this);
+        notifyAction.toStartListen();
+        notifyAction.toEnableFastInboxNotify();
     }
 
-    private Notification getNotification() {
-        RemoteInput remoteInput = new RemoteInput.Builder("REPLY")
-                .setLabel("REPLY")
-                .build();
-        PendingIntent replyIntent = PendingIntent.getActivity(this,
-                0,
-                getDirectReplyIntent(this, "REPLY"),
-                PendingIntent.FLAG_UPDATE_CURRENT);
-
-        NotificationCompat.Action replyAction =
-                new NotificationCompat.Action.Builder(R.drawable.bg_color,
-                        "REPLY", replyIntent)
-                        .addRemoteInput(remoteInput)
-                        .build();
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,notificationId)
-                .setSmallIcon(R.drawable.txt_color)
-                .setContentTitle("测试服务")
-                .setContentText("我正在运行")
-                .addAction(replyAction)
-                .setAutoCancel(true);
-        Notification notification = builder.build();
-        return notification;
-    }
-    private Intent getDirectReplyIntent(Context context, String label) {
-        /*return MessageActivity.getStartIntent(context)
-                .addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES)
-                .setAction(REPLY_ACTION)
-                .putExtra(CONVERSATION_LABEL, label);*/
-        return new Intent();
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        notifyAction.toStopListen();
+        notifyAction.toDisableFastInboxNotify();
     }
 }
