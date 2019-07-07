baa4ec17c063ebe614cbc0a13ec4dde62ae3dbc9
lw <184333220@qq.com>
2019-07-07 23:35:35 +0800

    添加多个todo子列表加载问题

--- LibAhtewlg7/src/main/java/ahtewlg7/utimer/mvp/BaseDeedListMvpP.java
+++ LibAhtewlg7/src/main/java/ahtewlg7/utimer/mvp/BaseDeedListMvpP.java
@@ -3,6 +3,7 @@ package ahtewlg7.utimer.mvp;
 import androidx.annotation.NonNull;
 
 import com.google.common.base.Optional;
+import com.google.common.collect.Lists;
 import com.trello.rxlifecycle2.LifecycleProvider;
 import com.trello.rxlifecycle2.android.FragmentEvent;
 import com.trello.rxlifecycle2.components.support.RxFragment;
@@ -14,8 +15,10 @@ import java.util.List;
 
 import ahtewlg7.utimer.comparator.DeedWarningTimeComparator;
 import ahtewlg7.utimer.entity.BaseEventBusBean;
+import ahtewlg7.utimer.entity.busevent.DeedBusEvent;
 import ahtewlg7.utimer.entity.gtd.GtdDeedEntity;
 import ahtewlg7.utimer.enumtype.DeedState;
+import ahtewlg7.utimer.enumtype.GtdBusEventType;
 import ahtewlg7.utimer.factory.EventBusFatory;
 import ahtewlg7.utimer.factory.GtdDeedByUuidFactory;
 import ahtewlg7.utimer.state.BaseGtdState;
@@ -36,79 +39,86 @@ public class BaseDeedListMvpP {
         this.mvpV = mvpV;
         mvpM      = new BaseDeedMvpM();
     }
-    public void toLoadDeedByState(final DeedState deedState){
+    public void toLoadDeedByState(final DeedState... deedState){
         mvpM.toLoad(deedState)
             .compose(((RxFragment)mvpV.getRxLifeCycleBindView()).<List<GtdDeedEntity>>bindUntilEvent(FragmentEvent.DESTROY))
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe(new MySafeSubscriber<List<GtdDeedEntity>>() {
+                List<GtdDeedEntity> allEntity = Lists.newArrayList();
                 @Override
                 public void onSubscribe(Subscription s) {
                     super.onSubscribe(s);
                     if(mvpV != null)
-                        mvpV.onLoadStart(deedState);
+                        mvpV.onLoadStart();
                 }
 
                 @Override
                 public void onNext(List<GtdDeedEntity> entityList) {
                     super.onNext(entityList);
-                    if(mvpV != null)
-                        mvpV.onLoadSucc(deedState, entityList);
+                    allEntity.addAll(entityList);
                 }
 
                 @Override
                 public void onError(Throwable t) {
                     super.onError(t);
                     if(mvpV != null)
-                        mvpV.onLoadErr(deedState, t);
+                        mvpV.onLoadErr(t);
                 }
 
                 @Override
                 public void onComplete() {
                     super.onComplete();
                     if(mvpV != null)
-                        mvpV.onLoadEnd(deedState);
+                        mvpV.onLoadSucc(allEntity);
                 }
             });
     }
 
     public void toTagDeed(final GtdDeedEntity deedEntity, final DeedState deedState, final int position){
         mvpM.toTag(deedEntity,deedState)
-                .compose(((RxFragment)mvpV.getRxLifeCycleBindView()).<Optional<BaseEventBusBean>>bindUntilEvent(FragmentEvent.DESTROY))
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new MySafeSubscriber<Optional<BaseEventBusBean>>() {
-                    @Override
-                    public void onSubscribe(Subscription s) {
-                        super.onSubscribe(s);
-                        if(mvpV != null)
-                            mvpV.onTagStart(deedEntity,deedState);
-                    }
+            .compose(((RxFragment)mvpV.getRxLifeCycleBindView()).<Optional<BaseEventBusBean>>bindUntilEvent(FragmentEvent.DESTROY))
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(new MySafeSubscriber<Optional<BaseEventBusBean>>() {
+                @Override
+                public void onSubscribe(Subscription s) {
+                    super.onSubscribe(s);
+                    if(mvpV != null)
+                        mvpV.onTagStart(deedEntity,deedState);
+                }
 
-                    @Override
-                    public void onNext(Optional<BaseEventBusBean> busBeanOptional) {
-                        super.onNext(busBeanOptional);
-                        if(mvpV == null)
-                            return;
-                        if(busBeanOptional.isPresent())
-                            mvpV.onTagSucc(deedEntity, deedState, position);
-                        else
-                            mvpV.onTagFail(deedEntity,deedState);
-                    }
+                @Override
+                public void onNext(Optional<BaseEventBusBean> busBeanOptional) {
+                    super.onNext(busBeanOptional);
+                    if(mvpV == null)
+                        return;
+                    if(busBeanOptional.isPresent())
+                        mvpV.onTagSucc(deedEntity, deedState, position);
+                    else
+                        mvpV.onTagFail(deedEntity,deedState);
+                }
 
-                    @Override
-                    public void onError(Throwable t) {
-                        super.onError(t);
-                        if(mvpV != null)
-                            mvpV.onTagErr(deedEntity,deedState, t);
-                    }
+                @Override
+                public void onError(Throwable t) {
+                    super.onError(t);
+                    if(mvpV != null)
+                        mvpV.onTagErr(deedEntity,deedState, t);
+                }
 
-                    @Override
-                    public void onComplete() {
-                        super.onComplete();
-                        if(mvpV != null)
-                            mvpV.onTagEnd(deedEntity,deedState);
-                    }
-                });
+                @Override
+                public void onComplete() {
+                    super.onComplete();
+                    if(mvpV != null)
+                        mvpV.onTagEnd(deedEntity,deedState);
+                }
+            });
     }
+    public void toHandleActionEvent(DeedBusEvent actionBusEvent, DeedState... state){
+        if(actionBusEvent == null || !actionBusEvent.ifValid())
+            return;
+        if(actionBusEvent.getEventType() == GtdBusEventType.LOAD)
+            toLoadDeedByState(state);
+    }
+
 
     class BaseDeedMvpM{
         Flowable<List<GtdDeedEntity>> toLoad(DeedState... state) {
@@ -164,10 +174,9 @@ public class BaseDeedListMvpP {
     }
     public interface IBaseDeedMvpV extends IRxLifeCycleBindView{
         public @NonNull LifecycleProvider getRxLifeCycleBindView();
-        public void onLoadStart(DeedState state);
-        public void onLoadSucc(DeedState state, List<GtdDeedEntity> entityList);
-        public void onLoadErr(DeedState state, Throwable err);
-        public void onLoadEnd(DeedState state);
+        public void onLoadStart();
+        public void onLoadSucc(List<GtdDeedEntity> entityList);
+        public void onLoadErr(Throwable err);
 
         public void onTagStart(GtdDeedEntity entity,DeedState toState);
         public void onTagSucc(GtdDeedEntity entity, DeedState toState, int position);
