eaee16fa0662d51107452f0f53dafa1905a9beca
lw <184333220@qq.com>
2019-07-19 20:19:15 +0800

    修改终止清单删除无效问题

--- AppUTimer/src/main/java/com/utimer/ui/DeedEndListFragment.java
+++ AppUTimer/src/main/java/com/utimer/ui/DeedEndListFragment.java
@@ -5,9 +5,13 @@ import android.view.View;
 
 import androidx.annotation.NonNull;
 
+import com.blankj.utilcode.util.ToastUtils;
 import com.utimer.R;
 import com.utimer.view.SimpleDeedRecyclerView;
 
+import java.util.Arrays;
+
+import ahtewlg7.utimer.entity.gtd.GtdDeedEntity;
 import ahtewlg7.utimer.enumtype.DeedState;
 import ahtewlg7.utimer.mvp.BaseDeedListMvpP;
 import ahtewlg7.utimer.util.MyRInfo;
@@ -15,6 +19,7 @@ import butterknife.BindView;
 
 import static ahtewlg7.utimer.enumtype.DeedState.DONE;
 import static ahtewlg7.utimer.enumtype.DeedState.TRASH;
+import static com.utimer.common.TagInfoFactory.INVALID_TAG_RID;
 
 public class DeedEndListFragment extends ADeedListFragment implements BaseDeedListMvpP.IBaseDeedMvpV {
     @BindView(R.id.fragment_deed_simple_list_recycler_view)
@@ -47,6 +52,17 @@ public class DeedEndListFragment extends ADeedListFragment implements BaseDeedLi
         return MyRInfo.getStringByID(R.string.title_deed_list_end);
     }
 
+    /**********************************************IBaseDeedMvpV**********************************************/
+    @Override
+    public void onTagSucc(GtdDeedEntity entity, DeedState toState, int position) {
+        if(toState != TRASH &&  Arrays.asList(workState).contains(toState))
+            recyclerView.resetData(position, entity);
+        else
+            recyclerView.removeData(entity);
+        int strRid = tagInfoFactory.getTagDetailRid(toState);
+        if(strRid != INVALID_TAG_RID)
+            ToastUtils.showShort(strRid);
+    }
     /**********************************************ADeedListFragment**********************************************/
     @NonNull
     @Override
