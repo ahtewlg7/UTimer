7e12395a486809933f97bda0423cacf5aaedef64
lw <184333220@qq.com>
2019-08-02 22:42:00 +0800

    优化deed 动态添加顺序

--- AppUTimer/src/main/java/com/utimer/ui/DeedScheduleListFragment.java
+++ AppUTimer/src/main/java/com/utimer/ui/DeedScheduleListFragment.java
@@ -7,9 +7,11 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.blankj.utilcode.util.ToastUtils;
+import com.google.common.base.Predicates;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
-import com.google.common.collect.Sets;
 import com.haibin.calendarview.Calendar;
 import com.haibin.calendarview.CalendarLayout;
 import com.haibin.calendarview.CalendarView;
@@ -17,12 +19,14 @@ import com.utimer.R;
 import com.utimer.mvp.ScheduleDeedListMvpP;
 import com.utimer.view.SimpleDeedRecyclerView;
 
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
 import org.joda.time.LocalDate;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
+import ahtewlg7.utimer.entity.busevent.DeedDoneBusEvent;
 import ahtewlg7.utimer.entity.gtd.GtdDeedEntity;
 import ahtewlg7.utimer.enumtype.DeedState;
 import ahtewlg7.utimer.factory.GtdDeedByUuidFactory;
@@ -43,7 +47,7 @@ public class DeedScheduleListFragment extends ADeedListFragment
     @BindView(R.id.fragment_deed_calendar_simple_list_recycler_view)
     SimpleDeedRecyclerView recyclerView;
 
-    private Set<GtdDeedEntity> deedEntitySet;
+    private List<GtdDeedEntity> deedEntityList;
     private Map<String, Calendar> deedCalendarMap;
 
     public static DeedScheduleListFragment newInstance() {
@@ -64,7 +68,7 @@ public class DeedScheduleListFragment extends ADeedListFragment
 
         showLifeInfo            = false;
         deedCalendarMap         = Maps.newHashMap();
-        deedEntitySet           = Sets.newLinkedHashSet();
+        deedEntityList          = Lists.newArrayList();
     }
 
     @Override
@@ -101,6 +105,11 @@ public class DeedScheduleListFragment extends ADeedListFragment
         return recyclerView;
     }
 
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onDeedDoneBusEvent(DeedDoneBusEvent eventBus) {
+        listMvpP.toHandleBusEvent(eventBus, getLoadDeedState());
+    }
+
     /**********************************************OnCalendarSelectListener**********************************************/
     @Override
     public void onCalendarOutOfRange(Calendar calendar) {
@@ -108,7 +117,7 @@ public class DeedScheduleListFragment extends ADeedListFragment
 
     @Override
     public void onCalendarSelect(Calendar calendar, boolean isClick) {
-        deedEntitySet.clear();
+        deedEntityList.clear();
         ((ScheduleDeedListMvpP)listMvpP).toLoadDeedByDate(calendar);
     }
     /**********************************************IBaseDeedMvpV**********************************************/
@@ -119,20 +128,20 @@ public class DeedScheduleListFragment extends ADeedListFragment
 
     @Override
     public void onLoadSucc(List<GtdDeedEntity> entityList) {
-        deedEntitySet.addAll(entityList);
-        getRecyclerView().resetData(Lists.newArrayList(deedEntitySet));
+        deedEntityList.addAll(entityList);
+        getRecyclerView().resetData(ImmutableSet.copyOf(Iterables.filter(deedEntityList, Predicates.not(Predicates.isNull()))).asList());
     }
 
     @Override
     public void onLoadSucc(GtdDeedEntity entity) {
-        deedEntitySet.add(entity);
-        getRecyclerView().resetData(Lists.newArrayList(deedEntitySet));
+        deedEntityList.add(0, entity);
+        getRecyclerView().resetData(ImmutableSet.copyOf(Iterables.filter(deedEntityList, Predicates.not(Predicates.isNull()))).asList());
     }
 
     @Override
     public void onTagSucc(GtdDeedEntity entity, DeedState toState, int position) {
         if(toState == DeedState.TRASH && !GtdDeedByUuidFactory.getInstance().ifExist(entity)){
-            deedEntitySet.remove(entity);
+            deedEntityList.remove(entity);
             getRecyclerView().removeData(entity);
         } else
             getRecyclerView().resetData(position, entity);
@@ -167,9 +176,7 @@ public class DeedScheduleListFragment extends ADeedListFragment
 
     @Override
     protected void toLoadDeedOnShow() {
-        if(mCalendarView.getSelectedCalendar() != null)
-            ((ScheduleDeedListMvpP)listMvpP).toLoadDeedByDate(mCalendarView.getSelectedCalendar());
-        else
-            listMvpP.toLoadDeedByDate(LocalDate.now());
+        mCalendarView.scrollToCurrent();
+        listMvpP.toLoadDeedByDate(LocalDate.now());
     }
 }
