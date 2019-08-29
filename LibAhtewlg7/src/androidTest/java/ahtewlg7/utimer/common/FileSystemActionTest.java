96804bcd2e1c0a0c16808372a885db509b152b9b
lw <184333220@qq.com>
2019-08-29 23:11:10 +0800

    添加androidjunit测试用例 (需要在Edit Configurations中添加Android Instrumented Tests)

---
+++ LibAhtewlg7/src/androidTest/java/ahtewlg7/utimer/common/FileSystemActionTest.java
@@ -0,0 +1,29 @@
+package ahtewlg7.utimer.common;
+
+
+import androidx.test.filters.RequiresDevice;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.File;
+
+@RunWith(AndroidJUnit4.class)
+public class FileSystemActionTest {
+    private FileSystemAction fileSystemAction = null;
+    @Before
+    public void setUp() throws Exception {
+        fileSystemAction = new FileSystemAction();
+    }
+
+    @Test
+    @RequiresDevice
+    public void listSubFile() {
+        File docFile = new File("/sdcard/UTimer/doc/notebook");
+        Iterable<File> subFile = fileSystemAction.listSubFile(docFile);
+        for(File file :subFile)
+            System.out.println(file.getAbsolutePath());
+    }
+}
